65b880e501492736758fc9c04af45288689331ea
792774502@qq.com <792774502@qq.com>
2025-01-29 18:57:47 +0800

    auto

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:auto_browser/features/browser/presentation/browser_screen.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-void main() => runApp(const AutoBrowserApp());
+void main() {
+  runApp(const AutoBrowserApp());
+}
 
 class AutoBrowserApp extends StatelessWidget {
   const AutoBrowserApp({super.key});
@@ -10,27 +12,131 @@ class AutoBrowserApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Auto Browser',
-      debugShowCheckedModeBanner: false,
-      theme: _buildIOSTheme(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: const BrowserScreen(),
     );
   }
+}
 
-  ThemeData _buildIOSTheme() {
-    return ThemeData(
-      useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.blue,
-        brightness: Brightness.light,
+class BrowserScreen extends StatefulWidget {
+  const BrowserScreen({super.key});
+
+  @override
+  _BrowserScreenState createState() => _BrowserScreenState();
+}
+
+class _BrowserScreenState extends State<BrowserScreen> {
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+  TextEditingController urlController = TextEditingController();
+  double progress = 0;
+  bool canGoBack = false;
+  bool canGoForward = false;
+
+  @override
+  void initState() {
+    super.initState();
+    urlController.text = 'https://www.baidu.com';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: urlController,
+          decoration: InputDecoration(
+            hintText: '输入网址',
+            contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+            suffixIcon: IconButton(
+              icon: Icon(progress < 1.0 ? Icons.close : Icons.refresh),
+              onPressed: () {
+                if (progress < 1.0) {
+                  webViewController?.stopLoading();
+                } else {
+                  webViewController?.reload();
+                }
+              },
+            ),
+          ),
+          onSubmitted: (value) => _loadUrl(value),
+          textInputAction: TextInputAction.go,
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: canGoBack
+                ? () {
+                    webViewController?.goBack();
+                  }
+                : null,
+          ),
+          IconButton(
+            icon: const Icon(Icons.arrow_forward),
+            onPressed: canGoForward
+                ? () {
+                    webViewController?.goForward();
+                  }
+                : null,
+          ),
+        ],
       ),
-      appBarTheme: const AppBarTheme(
-        centerTitle: false,
-        scrolledUnderElevation: 0.5,
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: Colors.blue.shade100,
-        foregroundColor: Colors.blue.shade800,
+      body: Column(
+        children: [
+          LinearProgressIndicator(
+            value: progress,
+            backgroundColor: Colors.transparent,
+          ),
+          Expanded(
+            child: InAppWebView(
+              key: webViewKey,
+              initialUrlRequest: URLRequest(url: WebUri(urlController.text)),
+              onWebViewCreated: (controller) {
+                webViewController = controller;
+              },
+              onLoadStart: (controller, url) {
+                setState(() {
+                  urlController.text = url?.toString() ?? '';
+                });
+              },
+              onLoadStop: (controller, url) {
+                setState(() {
+                  urlController.text = url?.toString() ?? '';
+                });
+              },
+              onProgressChanged: (controller, progress) {
+                setState(() {
+                  this.progress = progress / 100;
+                });
+              },
+              onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                _updateNavigationState();
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void _loadUrl(String url) {
+    final uri = WebUri(url);
+    if (uri.scheme.isEmpty) {
+      webViewController?.loadUrl(
+          urlRequest: URLRequest(url: WebUri('https://$url')));
+    } else {
+      webViewController?.loadUrl(urlRequest: URLRequest(url: uri));
+    }
+  }
+
+  void _updateNavigationState() async {
+    final canBack = await webViewController?.canGoBack() ?? false;
+    final canForward = await webViewController?.canGoForward() ?? false;
+    setState(() {
+      canGoBack = canBack;
+      canGoForward = canForward;
+    });
   }
 }
