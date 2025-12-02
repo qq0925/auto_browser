f5a477954e9ba79e87c8c63ed8d70fdee4b46741
792774502@qq.com <792774502@qq.com>
2025-12-02 11:29:50 +0800

    added

--- lib/providers/browser_provider.dart
+++ lib/providers/browser_provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'dart:io';
 import '../models/browser_tab.dart';
